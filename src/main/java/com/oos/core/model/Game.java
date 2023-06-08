c9efb739a00d6b200dbb7860b2238439a65404df
Oluwaseun Smart <smartunlimited95@gmail.com>
2023-06-08 19:20:51 +0200

    updated java version

--- src/main/java/com/oos/core/model/Game.java
+++ src/main/java/com/oos/core/model/Game.java
@@ -1,4 +1,13 @@
 package com.oos.core.model;
 
+import java.time.LocalDateTime;
+
 public class Game {
+
+    private int id;
+    private Team home;
+    private Team away;
+    private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime endAt;
 }
