b54a2b987c5a6766d71bdabebc2108bda7bc08cd
Oluwaseun Smart <smartunlimited95@gmail.com>
2023-06-08 19:56:40 +0200

    added implementation to update game and get a game list

--- src/main/java/com/oos/core/model/ScoreBoard.java
+++ src/main/java/com/oos/core/model/ScoreBoard.java
@@ -1,7 +1,11 @@
 package com.oos.core.model;
 
+import com.oos.exceptions.GameNotFoundException;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class ScoreBoard {
 
@@ -14,17 +18,23 @@ public class ScoreBoard {
     }
 
     public void update(int id, int home, int away) {
-
+        find(id).update(home, away);
     }
 
     public void finish(int id) {
     }
 
     public List<Game> getGames() {
-        return new ArrayList<>();
+        Collections.sort(games);
+        return games;
     }
 
     public String getSummary() {
         return "";
+    }
+
+    private Game find(int id) {
+        Optional<Game> optional = games.stream().filter(e -> e.getId() == id).findFirst();
+        return optional.orElseThrow(() -> new GameNotFoundException("Game with id " + id + " not found"));
     }
 }
--- src/test/java/com/oos/core/model/ScoreBoardTest.java
+++ src/test/java/com/oos/core/model/ScoreBoardTest.java
@@ -2,6 +2,8 @@ package com.oos.core.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScoreBoardTest {
@@ -12,12 +14,21 @@ public class ScoreBoardTest {
         ScoreBoard board = new ScoreBoard();
         final int gameId = board.start(1, "Poland", "Italy");
         assertEquals(gameId, 1);
+        assertEquals(board.getGames().size(), 1);
     }
 
     @Test
     void update() {
         ScoreBoard board = new ScoreBoard();
+        board.start(1, "Poland", "Italy");
         board.update(1, 6, 2);
+
+        final Optional<Game> optional = board.getGames().stream().filter(g -> g.getId() == 1).findFirst();
+        if (optional.isPresent()) {
+            final Game game = optional.get();
+            assertEquals(game.getHome().getScore(), 6);
+            assertEquals(game.getAway().getScore(), 2);
+        }
     }
 
     @Test
