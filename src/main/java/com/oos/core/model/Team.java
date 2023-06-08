42591b516543a1f231a7ba53a529a080b0f7b32d
Oluwaseun Smart <smartunlimited95@gmail.com>
2023-06-08 20:09:46 +0200

    removed unused methods

--- src/main/java/com/oos/core/model/Team.java
+++ src/main/java/com/oos/core/model/Team.java
@@ -16,23 +16,11 @@ public class Team {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getScore() {
         return score;
     }
 
     public void setScore(int score) {
         this.score = score;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
     }
 }
--- src/main/java/com/oos/exceptions/InvalidStateException.java
+++ src/main/java/com/oos/exceptions/InvalidStateException.java
@@ -1,24 +1,7 @@
 package com.oos.exceptions;
 
 public class InvalidStateException extends RuntimeException{
-
-    public InvalidStateException() {
-        super();
-    }
-
     public InvalidStateException(String message) {
         super(message);
-    }
-
-    public InvalidStateException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InvalidStateException(Throwable cause) {
-        super(cause);
-    }
-
-    protected InvalidStateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
